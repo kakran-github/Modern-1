e9d2467c3dcf6487af107a67a0c76b8c3e5f96ac
Yuvraj Chaudhary <yuvr4aj@gmail.com>
2024-02-20 19:43:48 +0530

    smoothscroll

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 var now = new Date();
-var datetime = now.toLocaleString();
-document.getElementById("datetime").innerHTML = datetime;
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var ampm = hours >= 12 ? 'PM' : 'AM'; // Determine if it's AM or PM
+
+// Convert hours to 12-hour format
+hours = hours % 12;
+hours = hours ? hours : 12; // Handle midnight (0 hours)
+
+// Add leading zeros if necessary
+// hours = (hours < 10) ? '0' + hours : hours;
+minutes = (minutes < 10) ? '0' + minutes : minutes;
+
+var timeString = hours + ':' + minutes + ' ' + ampm + ' EST'; // Construct time string
+document.getElementById("time").innerHTML = timeString; // Display time
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
