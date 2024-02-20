1501c5e168d2a9804791413e2ce564c63c369c93
Yuvraj Chaudhary <yuvr4aj@gmail.com>
2024-02-21 00:17:27 +0530

    minicursor

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-var now = new Date();
-var hours = now.getHours();
-var minutes = now.getMinutes();
-var ampm = hours >= 12 ? 'PM' : 'AM'; // Determine if it's AM or PM
+// 1. Adding real time
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM'; // Determine if it's AM or PM
+  
+  // Convert hours to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Handle midnight (0 hours)
+  
+  // Add leading zeros if necessary
+  // hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  
+  var timeString = hours + ':' + minutes + ' ' + ampm + ' EST'; // Construct time string
+  document.getElementById("time").innerHTML = timeString; // Display time
 
-// Convert hours to 12-hour format
-hours = hours % 12;
-hours = hours ? hours : 12; // Handle midnight (0 hours)
+// 2. Adding smooth scroll to main    
+  const scroll = new LocomotiveScroll({
+      el: document.querySelector('#main'),
+      smooth: true
+  });
 
-// Add leading zeros if necessary
-// hours = (hours < 10) ? '0' + hours : hours;
-minutes = (minutes < 10) ? '0' + minutes : minutes;
+// 3. Making moving minicursor
+   var crsr=document.querySelector("#minicircle");
 
-var timeString = hours + ':' + minutes + ' ' + ampm + ' EST'; // Construct time string
-document.getElementById("time").innerHTML = timeString; // Display time
+// window.addEventListener("mousemove",function(dets){
+//         crsr.style.left=dets.x+"px";
+//         crsr.style.top=dets.y+"px";
+// });
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// or
+
+   function mousefollower(){
+       window.addEventListener("mousemove",function(dets){
+           crsr.style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+       })
+   }
+   mousefollower()
+
+// 4. 
